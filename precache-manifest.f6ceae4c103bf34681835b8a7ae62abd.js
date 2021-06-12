83302a4b3f12e0a688743f83d88200b7e704530c
Code Sandbox <hello@codesandbox.io>
2021-06-12 01:23:23 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f6ceae4c103bf34681835b8a7ae62abd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed4530d22ae2820189a7c577c09c5e1",
+    "url": "https://amam789.github.io/csb-4ww2e/index.html"
+  },
+  {
+    "revision": "26cc8b488cabc6e03421",
+    "url": "https://amam789.github.io/csb-4ww2e/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "4921b688a4e5898fc77f",
+    "url": "https://amam789.github.io/csb-4ww2e/static/js/2.97006be8.chunk.js"
+  },
+  {
+    "revision": "26cc8b488cabc6e03421",
+    "url": "https://amam789.github.io/csb-4ww2e/static/js/main.4a42ca30.chunk.js"
+  },
+  {
+    "revision": "1be24d39cf00e2e9283e",
+    "url": "https://amam789.github.io/csb-4ww2e/static/js/runtime~main.1e4c82eb.js"
+  }
+]);
